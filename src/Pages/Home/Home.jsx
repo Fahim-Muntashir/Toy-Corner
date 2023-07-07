167bd8cc37867dedde6607d915fb733d27bc6377
FahimMuntashir1 <fahimasdf07@gmail.com>
2023-07-07 14:38:23 +0600

    tab added

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import BestDeal from "./BestDeal/BestDeal";
 import Banner from "./Banner/Banner";
 import Gallery from "./Gallary/Gallary";
+import Tabs from "./Tabs/Tabs";
 
 const Home = () => {
   return (
     <div>
       <Banner></Banner>
       <Gallery></Gallery>
+      <Tabs></Tabs>
       <BestDeal></BestDeal>
     </div>
   );
